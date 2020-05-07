8324f4cd7aeb972695ef5c759729674aeea1befb
ColibriStudio <35774154+ColibriStudioDev@users.noreply.github.com>
2020-05-07 15:35:41 +0200

    Add files via upload

---
+++ CSVFile.cs
@@ -0,0 +1,124 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CSVFile
+{
+    public List<string[]> lines = new List<string[]>();
+    public List<string> cells;
+
+    public string savePath;
+    public CSVFile(string path)
+    {
+        savePath = path;
+        Parsing(File.ReadAllLines(path));
+    }
+    public CSVFile(TextAsset asset)
+    {
+        Parsing(asset.text.Split('\n'));
+    }
+
+
+
+    //INTERNAL
+    private void Parsing(string[] allLines)
+    {
+        foreach (string line in allLines)
+        {
+            string[] cellsOnLine = line.Split(',');
+            cells = new List<string>();
+            foreach (string tempCell in cellsOnLine)
+            {
+                cells.Add(tempCell);
+
+            }
+
+            lines.Add(cells.ToArray());
+        }
+
+
+
+    }
+    //PUBLIC
+    public string GetRawText()
+    {
+        string rawtext="";
+
+        for(int i = 0; i < lines.Count; i++)
+        {
+            for(int z = 0; z < lines[i].Length; z++)
+            {
+                rawtext += lines[i][z];
+                if (z < lines[i].Length - 1) rawtext += ',';
+            }
+
+            if (i < lines.Count - 1) rawtext += '\n';
+        }
+
+        return rawtext;
+    }
+    public string[] GetAllCells()
+    {
+        List<string> temp = new List<string>();
+        foreach(string[] line in lines)
+        {
+            foreach(string cell in line)
+            {
+                temp.Add(cell);
+            }
+        }
+
+
+        return temp.ToArray();
+    }
+    public void AddLine()
+    {
+        cells = new List<string>();
+        for(int i = 0; i < lines[lines.Count -1].Length; i++)
+        {
+            cells.Add("none");
+        }
+        lines.Add(cells.ToArray());
+
+        
+    }
+    public void AddLine(int index)
+    {
+        cells = new List<string>();
+        for (int i = 0; i < lines[lines.Count - 1].Length; i++)
+        {
+            cells.Add("none");
+        }
+        lines.Insert(index,cells.ToArray());
+
+
+    }
+    public void RemoveLine(int index)
+    {
+        lines.RemoveAt(index);
+    }
+    public void Save()
+    {
+        File.WriteAllText(savePath, GetRawText());
+    }
+    public void Save(string path)
+    {
+        File.WriteAllText(path, GetRawText());
+    }
+
+    public Dictionary<string,string> GetDictionary(int cellKeyIndex,int cellValueIndex)
+    {
+        Dictionary<string, string> temp = new Dictionary<string, string>();
+        foreach(string[] line in lines)
+        {
+            if (cellValueIndex > line.Length - 1 || cellKeyIndex > line.Length - 1) return null;
+            temp.Add(line[cellKeyIndex], line[cellValueIndex]);
+        }
+        return temp;
+    }
+    
+
+}
+
+
---
+++ CSVModifierWindows.cs
@@ -0,0 +1,126 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.AssetImporters;
+using System.IO;
+
+public class CSVModifierWindows : EditorWindow
+{
+
+    [MenuItem("CustomWindows/CSV Modifier")]
+    public static void ShowWindows()
+    {
+        GetWindow<CSVModifierWindows>("CSV Modifier");
+    }
+
+
+    private enum WindowsState
+    {
+        Nothing,
+        Editing,
+    }
+    WindowsState state = WindowsState.Nothing;
+
+    //INTERNAL VARIABLE
+    CSVFile currentEditingFile = null;
+    TextAsset rawText = null;
+
+    bool hasbeenLoad = false;
+
+
+    //LINE PARAMETER
+    int removingLineIndex = 0;
+    int insertingIndex = -1;
+    private void OnGUI()
+    {
+        TextAsset previousone = rawText;
+        if(state == WindowsState.Nothing)
+        {
+            rawText = (TextAsset)EditorGUILayout.ObjectField("CSV file to edit", rawText, typeof(TextAsset),false);
+
+
+        }
+        else if(state == WindowsState.Editing)
+        {
+            rawText = (TextAsset)EditorGUILayout.ObjectField("CSV file to edit", rawText, typeof(TextAsset), false);
+#region MODIFY BAR
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Add Line At (-1 to auto add at the end)"))
+            {
+                AddLine();
+            }
+            insertingIndex = EditorGUILayout.IntField(insertingIndex,GUILayout.MaxWidth(30));
+            if(GUILayout.Button("Remove Line At"))
+            {
+                RemoveLine();
+            }
+            removingLineIndex = EditorGUILayout.IntField(removingLineIndex, GUILayout.MaxWidth(30));
+
+            GUILayout.EndHorizontal();
+#endregion
+            GUILayout.Space(20);
+
+            for (int i = 0; i < currentEditingFile.lines.Count; i++) {
+#region ITEM
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(i.ToString());
+                for(int z = 0; z < currentEditingFile.lines[i].Length; z++)
+                {
+  
+                    currentEditingFile.lines[i][z] = EditorGUILayout.TextField(currentEditingFile.lines[i][z]);
+                }
+                GUILayout.EndHorizontal();
+#endregion
+
+            }
+
+
+            GUILayout.Space(30);
+#region SAVING
+            if(GUILayout.Button("Apply Changes"))
+            {
+                Apply();
+            }
+#endregion
+        }
+
+        if (rawText != previousone)
+        {
+   
+            Init();
+            state = WindowsState.Editing;
+        }
+        if(rawText == null)
+        {
+            state = WindowsState.Nothing;
+        }
+    }
+
+    private void Init()
+    {
+        currentEditingFile = new CSVFile(rawText);
+
+    }
+
+    private void Apply()
+    {
+        currentEditingFile.Save(AssetDatabase.GetAssetPath(rawText));
+       
+    }
+
+    private void AddLine()
+    {
+        if (insertingIndex == -1) currentEditingFile.AddLine();
+        else currentEditingFile.AddLine(insertingIndex);
+
+    }
+
+    private void RemoveLine()
+    {
+        currentEditingFile.RemoveLine(removingLineIndex);
+    }
+
+}
+#endif
